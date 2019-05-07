1a3c027852554251ecbc8b54394551ecfc7b3b8c
Poczok <tamaspece@gmail.com>
2019-05-07 16:58:15 +0200

    JFrame added, JLabels 10x10 has been made

---
+++ Java/StrategyGameSecondHomeProject/src/GameArea.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameArea extends JFrame {
+    public GameArea() {
+        super("GameArea");
+        this.setSize(1000,600);
+        setLayout(new BorderLayout());
+        JLabel RedPlayerLife = new JLabel("Red players life: 200");
+        JLabel BluePlayerLife = new JLabel("Blue players life: 200");
+        JPanel playerStatus = new JPanel();
+        JPanel map = new JPanel(new GridLayout(10,10));
+        this.add(showGUI(map), BorderLayout.CENTER);
+        playerStatus.add(RedPlayerLife);
+        playerStatus.add(BluePlayerLife);
+        this.add(playerStatus, BorderLayout.SOUTH);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    private JLabel[] createLabels() {
+        JLabel[] labels  = new JLabel[100];
+        for(int i=0; i<100; i++) {
+            labels[i] = new JLabel("field: " + (i+1));
+        }
+        return labels;
+    }
+    private JPanel showGUI(JPanel map) {
+        JLabel[] labels = createLabels();
+        for(int i=0; i < labels.length; i++) {
+            map.add(labels[i]);
+        }
+        return map;
+    }
+}
---
+++ Java/StrategyGameSecondHomeProject/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        GameArea garea = new GameArea();
+    }
+}
