8ed6cea9310becbdae9df00e4b9dc3af6c0dcb04
Poczok <tamaspece@gmail.com>
2019-05-09 20:13:24 +0200

    fibonacci with recursion

---
+++ Java/RecursionPractice - Fibonacci/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static int a = 1;
+    public static int b = 2;
+    public static int result = 3;
+    public static int temp = 0;
+    public static void main(String[] args) {
+        System.out.println(fibonacci(3));
+    }
+    public static int fibonacci(int n) {
+        if(n==0) {
+            return 0;
+        } else if(n==1 || n==2) {
+            return 1;
+        }
+        temp = b;
+        b = b + a;
+        a = temp;
+        fibonacci(n-1);
+        return a;
+    }
+}
+/*
+The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition.
+The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of
+the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive
+fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence.
+
+fibonacci(0) → 0
+fibonacci(1) → 1
+fibonacci(2) → 1
+ */
